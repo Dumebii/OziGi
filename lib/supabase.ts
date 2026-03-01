9d28d96dff218bda2bbcdac11d9bab61eb159384
Dumebi Okolo <okolodumebi@gmail.com>
2026-03-01 16:15:01 +0000

    create supabase client

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// This client handles both Auth and Database interactions
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
